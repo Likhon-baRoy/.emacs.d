0d2df907d9a96ea13b09cb59a6ee88c42f767128
Likhon Barai <likhonhere007@gmail.com>
2022-11-03 20:12:31 +0600

    removed lsp-server

--- var/lsp/session.el
+++
@@ -1,1 +0,0 @@
-#s(lsp-session ("/home/raxit/wrx") ("/home/raxit/") #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()) #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()) #s(hash-table size 65 test equal rehash-size 1.5 rehash-threshold 0.8125 data ()))
